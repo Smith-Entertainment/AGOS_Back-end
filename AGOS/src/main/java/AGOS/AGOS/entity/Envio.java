274fb0a6b7fc2ae08ad893cb4052eabc5f3a9518
Cleyton da Silva Pires <cleytonxsilva@outlook.com>
2023-04-04 18:44:00 -0300

    classes adicionadas

--- AGOS/src/main/java/AGOS/AGOS/entity/Envio.java
+++ AGOS/src/main/java/AGOS/AGOS/entity/Envio.java
@@ -4,27 +4,29 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
-@Table(name = "Documento", schema = "public")
-public class Documento {
+@Table(name = "Envio", schema = "public")
+public class Envio {
     @Id
     @Getter
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id",nullable = false, unique = true)
+    @Column(name = "id",nullable = false,unique = true)
     private Long id;
-
-    @Enumerated(EnumType.STRING)
     @Getter @Setter
-    @Column(name = "grupo_projeto")
-    private GrupoProjeto grupoProjeto;
+    @Column(name = "data")
+    private LocalDate data;
     @Getter @Setter
-    @OneToMany
-    @JoinTable(name = "arquivos")
-    private List<Arquivo> arquivos;
+    @JoinTable(name = "usuarios")
+    private List<Usuario> = new usuarios;
+    @Getter @Setter
+    @JoinTable(name = "items")
+    private List<Item> = new items;
     @ManyToOne
     @Getter @Setter
     @JoinColumn(name = "obra")
     private Obra obra = new Obra();
+
 }
--- AGOS/src/main/java/AGOS/AGOS/entity/Obra.java
+++ AGOS/src/main/java/AGOS/AGOS/entity/Obra.java
@@ -23,11 +23,11 @@ public class Obra {
     @Column(name= "licitacao",length = 50,nullable = false, unique = true)
     private String licitacao;
     @Getter @Setter
-    @Column(name= "dataCertame",nullable = false)
-    private LocalDate data_certame;
+    @Column(name= "data_certame",nullable = false)
+    private LocalDate dataCertame;
     @Getter @Setter
-    @Column(name= "valorEdital",nullable = false)
-    private double valor_edital;
+    @Column(name= "valor_edital",nullable = false)
+    private double valorEdital;
     @Getter @Setter
     @Column(name= "bairro",length = 50,nullable = false)
     private String bairro;
@@ -38,25 +38,25 @@ public class Obra {
     @Column(name= "numero",nullable = false)
     private int numero;
     @Getter @Setter
-    @Column(name= "valorContratado",length = 50)
-    private double valor_contratado;
+    @Column(name= "valor_contratado",length = 50)
+    private double valorContratado;
     @Getter @Setter
-    @Column(name= "dataInicio")
-    private LocalDate data_inicio;
+    @Column(name= "data_inicio")
+    private LocalDate dataInicio;
     @Getter @Setter
-    @Column(name= "dataTermino")
-    private LocalDate data_termino;
+    @Column(name= "data_termino")
+    private LocalDate dataTermino;
     @Getter @Setter
-    @Column(name= "numeroContrato")
-    private int numero_contrato;
+    @Column(name= "numero_contrato")
+    private int numeroContrato;
     @Getter @Setter
-    @Column(name= "empresaContratada",length = 30)
-    private String empresa_contratada;
+    @Column(name= "empresa_contratada",length = 30)
+    private String empresaContratada;
     @Getter @Setter
-    @Column(name= "finalizado")
+    @Column(name= "finalizado", nullable = false)
     private boolean finalizado;
     @Enumerated(EnumType.STRING)
     @Getter @Setter
-    @Column(name= "tipoObra")
-    private Tipo_obra tipo;
+    @Column(name= "tipo_obra", nullable = false)
+    private Tipo_obra tipoObra;
 }
