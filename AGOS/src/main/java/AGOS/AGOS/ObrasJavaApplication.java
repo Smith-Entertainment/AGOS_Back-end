8af6252906e71a1b5924a3bd1d7a6d1a2ab84c65
AndreiBack <andreigregoriback@gmail.com>
2023-04-20 20:24:59 -0300

    classes e repository

--- AGOS/src/main/java/AGOS/AGOS/ObrasJavaApplication.java
+++ AGOS/src/main/java/AGOS/AGOS/ObrasJavaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ObrasJavaApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ObrasJavaApplication.class, args);
 	}
