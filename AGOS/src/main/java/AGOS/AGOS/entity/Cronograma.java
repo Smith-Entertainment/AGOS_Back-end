a378ac7a31e4677569168cb2d1ac4beda5855484
frederico-enzo <117025457+frederico-enzo@users.noreply.github.com>
2023-04-28 12:53:05 -0300

    add class cronograma e tirei class previsto e realizado

---
+++ AGOS/src/main/java/AGOS/AGOS/entity/Cronograma.java
@@ -0,0 +1,53 @@
+package AGOS.AGOS.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_cronograma", schema = "public")
+public class Cronograma {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Getter @Setter
+    @ManyToOne(fetch =FetchType.LAZY ,cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_periodo_mes")
+    private Periodo mes;
+
+    @Getter @Setter
+    @ManyToOne(fetch =FetchType.LAZY ,cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_obra")
+    private Obra obra;
+
+    @Getter @Setter
+    @ManyToOne(fetch =FetchType.LAZY ,cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_item")
+    private Item item;
+
+    @Getter @Setter
+    @Column(name = "previsto_financeiro")
+    private BigDecimal previstoFinanceiro;
+
+    @Getter @Setter
+    @Column(name = "previsto_fisico")
+    private BigDecimal previstoFisico;
+
+    @Getter @Setter
+    @Column(name = "realizado_financeiro")
+    private BigDecimal realizadoFinanceiro;
+
+    @Getter @Setter
+    @Column(name = "realizado_fisico")
+    private BigDecimal realizadoPrevistoFisico;
+
+
+
+
+
+}
---
+++ AGOS/src/main/java/AGOS/AGOS/repository/CronogramaRepository.java
@@ -0,0 +1,7 @@
+package AGOS.AGOS.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CronogramaRepository extends JpaRepository<Comparable, Long> {
+
+}
