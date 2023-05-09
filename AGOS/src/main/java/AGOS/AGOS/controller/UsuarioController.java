08df7d1aea89945d78abc7a30a89b8b441f71246
andre <andremaivelasco@hotmail.com>
2023-05-09 19:32:07 -0300

    base controller feita

--- AGOS/src/main/java/AGOS/AGOS/controller/UsuarioController.java
+++ AGOS/src/main/java/AGOS/AGOS/controller/UsuarioController.java
@@ -1,7 +1,46 @@
 package AGOS.AGOS.controller;
 
+import AGOS.AGOS.entity.Usuario;
+import AGOS.AGOS.services.UsuarioService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/usuario")
 public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<?> findById(@RequestParam("id") final Long id) {
+        final Usuario usuario = this.usuarioService.findById(id);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @GetMapping("/lista")
+    public ResponseEntity<?> findAll() {
+        final Usuario usuario = this.usuarioService.findById(id);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> cadastrar(@RequestBody final Usuario usuario) {
+        this.usuarioService.cadastrar(usuario);
+        return ResponseEntity.ok("Registro cadastrado com sucesso");
+    }
+
+    @PutMapping
+    public ResponseEntity<?> editar(@RequestParam("id") final Long id, @RequestBody final Usuario usuario) {
+        this.usuarioService.editar(usuario);
+        return ResponseEntity.ok("Registro editado com sucesso");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> excluir(@RequestParam("id") final Long id){
+        this.usuarioService.excluir(id);
+        return ResponseEntity.ok("Registro excluido com sucesso");
+    }
 }
