b27e6a3161112a5b2cd84fb40722c98ba55241a5
frederico-enzo <117025457+frederico-enzo@users.noreply.github.com>
2023-05-09 15:22:06 -0300

    Comecei a fazer a controller de Item SO fiz findById por enquanto

--- AGOS/src/main/java/AGOS/AGOS/controller/ItemController.java
+++ AGOS/src/main/java/AGOS/AGOS/controller/ItemController.java
@@ -1,4 +1,33 @@
 package AGOS.AGOS.controller;
 
+
+import AGOS.AGOS.entity.Item;
+import AGOS.AGOS.repository.ItemRepository;
+import AGOS.AGOS.services.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/obra/item")
 public class ItemController {
+
+    @Autowired
+    private ItemRepository itemRepository;
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findByIdPath(@PathVariable("id") final Long id){
+        final Item item = this.itemRepository.findById(id).orElse(null);
+
+        return item == null
+                ? ResponseEntity.badRequest().body("Item não encontrado")
+                : ResponseEntity.ok(item);
+    }
+
+
 }
