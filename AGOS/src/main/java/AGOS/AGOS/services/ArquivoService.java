5e34ddd23705ee7386bada4fae096c891f14b461
Cleyton da Silva Pires <cleytonxsilva@outlook.com>
2023-05-11 20:52:09 -0300

    findbyid e findyall na servicecontroller

--- AGOS/src/main/java/AGOS/AGOS/services/ArquivoService.java
+++ AGOS/src/main/java/AGOS/AGOS/services/ArquivoService.java
@@ -1,13 +1,24 @@
 package AGOS.AGOS.services;
 
+import AGOS.AGOS.entity.Arquivo;
 import AGOS.AGOS.repository.ArquivoRepository;
 import org.hibernate.envers.Audited;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ArquivoService {
     @Autowired
     ArquivoRepository arquivoRepository;
 
+    public Optional<Arquivo> findById(Long id) {
+        return arquivoRepository.findById(id);
+    }
+
+    public List<Arquivo> findAll() {
+        return arquivoRepository.findAll();
+    }
 }
