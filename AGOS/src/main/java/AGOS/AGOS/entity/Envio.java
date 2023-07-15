3e10bda5ab747977a2a45eee9995e6a7eb594f61
Luis <luiserr1394@gmail.com>
2023-07-15 13:48:12 -0300

    resolvido problema com lista de envio

--- AGOS/src/main/java/AGOS/AGOS/entity/Envio.java
+++ AGOS/src/main/java/AGOS/AGOS/entity/Envio.java
@@ -21,9 +21,9 @@ public class Envio {
     private LocalDate data;
 
     @Getter @Setter
-    @ManyToMany(fetch =FetchType.LAZY ,cascade = CascadeType.ALL)
+    @ManyToOne(fetch =FetchType.LAZY ,cascade = CascadeType.ALL)
     @JoinColumn(name = "id_usuario")
-    private List<Usuario> voluntarios;
+    private Usuario voluntario;
 
     @Getter @Setter
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
