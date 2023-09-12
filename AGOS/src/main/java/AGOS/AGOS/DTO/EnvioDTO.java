175f1634e4b24ddfa46a12a9941e2b10fceba08c
Luis Rodriguez <luiserr1394@gmail.com>
2023-09-11 22:33:58 -0300

    Criando EnvioDTO

---
+++ AGOS/src/main/java/AGOS/AGOS/DTO/EnvioDTO.java
@@ -0,0 +1,26 @@
+package AGOS.AGOS.DTO;
+
+import AGOS.AGOS.entity.Item;
+import AGOS.AGOS.entity.Obra;
+import AGOS.AGOS.entity.Usuario;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class EnvioDTO {
+
+
+    private Long id;
+    private Usuario voluntario;
+    private List<Item> item;
+    private Obra obra;
+
+
+
+}
