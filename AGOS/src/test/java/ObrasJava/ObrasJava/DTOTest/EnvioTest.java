a8fdb50cb4ca6e0ac91c43f2d0a8c209e0bb2f5a
Luis Rodriguez <luiserr1394@gmail.com>
2023-09-30 18:10:15 -0300

    Testes Entity Envio

---
+++ AGOS/src/test/java/ObrasJava/ObrasJava/DTOTest/EnvioTest.java
@@ -0,0 +1,17 @@
+package ObrasJava.ObrasJava.DTOTest;
+
+
+import AGOS.AGOS.config.WebConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = WebConfig.class)
+public class EnvioTest {
+
+
+
+
+
+
+
+
+}
