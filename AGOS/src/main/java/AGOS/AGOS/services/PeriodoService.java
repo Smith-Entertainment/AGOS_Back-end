0f21263e1de730a920d17f45d983ffce444c8695
Luis <luiserr1394@gmail.com>
2023-11-20 20:01:11 -0300

    corrigindo erro nas dependencias

--- AGOS/src/main/java/AGOS/AGOS/services/PeriodoService.java
+++ AGOS/src/main/java/AGOS/AGOS/services/PeriodoService.java
@@ -1,73 +1,57 @@
-    package AGOS.AGOS.services;
+package AGOS.AGOS.services;
 
-    import AGOS.AGOS.DTO.PeriodoDTO;
-    import AGOS.AGOS.entity.Periodo;
-    import AGOS.AGOS.repository.PeriodoRepository;
-    import org.modelmapper.ModelMapper;
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.stereotype.Service;
-    import org.springframework.transaction.annotation.Transactional;
-
+import AGOS.AGOS.DTO.PeriodoDTO;
+import AGOS.AGOS.entity.Periodo;
+import AGOS.AGOS.repository.PeriodoRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
 public class PeriodoService {
-
     @Autowired
     private PeriodoRepository periodoRepository;
-
-    @Transactional(rollbackFor = Exception.class)
-    public Periodo findById(Long id){
-        final Periodo periodo = this.periodoRepository.findById(id)
-                .orElseThrow(()-> new IllegalArgumentException("Periodo não encontrado"));
-        return periodo;
+    @Autowired
+    private ModelMapper modelMapper;
+    Periodo toPeriodo(PeriodoDTO periodoDTO){
+        return modelMapper.map(periodoDTO, Periodo.class);
     }
-
-
-    @Transactional(rollbackFor = Exception.class)
-    public Periodo create (Periodo periodo){
-        int ano = periodo.getAno();
+    PeriodoDTO toPeriodoDTO (Periodo periodo){
+        return modelMapper.map(periodo, PeriodoDTO.class);
+    }
+    void isNull(Long id ){
+        periodoRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("Periodo não encontrado"));
+    }
+    void validationPeriodoDTO(PeriodoDTO periodoDTO){
+        int ano = periodoDTO.getAno();
         String anoString = Integer.toString(ano);
-
         if (!anoString.matches("[0-9]{4}")){
             throw new IllegalArgumentException("Ano inválido");
-
-
         }
-         PeriodoDTO toPeriodoDTO (Periodo periodo){
-            return modelMapper.map(periodo, PeriodoDTO.class);
-        }
-         void isNull(Long id ){
-            periodoRepository.findById(id)
-                    .orElseThrow(()-> new IllegalArgumentException("Periodo não encontrado"));
-        }
-         void validationPeriodoDTO(PeriodoDTO periodoDTO){
-            int ano = periodoDTO.getAno();
-            String anoString = Integer.toString(ano);
-            if (!anoString.matches("[0-9]{4}")){
-                throw new IllegalArgumentException("Ano inválido");
-            }
-        }
-        public PeriodoDTO findById(Long id){
-            isNull(id);
-            final Periodo periodo = this.periodoRepository.findById(id).orElse(null);
-            return toPeriodoDTO(periodo);
-        }
-        @Transactional(rollbackFor = Exception.class)
-        public PeriodoDTO create (PeriodoDTO periodo){
-            validationPeriodoDTO(periodo);
-            return toPeriodoDTO(periodoRepository.save(toPeriodo(periodo)));
-        }
-        @Transactional(rollbackFor = Exception.class)
-        public PeriodoDTO update (PeriodoDTO periodo){
-            isNull(periodo.getId());
-            validationPeriodoDTO(periodo);
-            return toPeriodoDTO(periodoRepository.save(toPeriodo(periodo)));
-        }
-        @Transactional(rollbackFor = Exception.class)
-        public void delete(Long id){
-            isNull(id);
-            periodoRepository.deleteById(id);
-        }
-
     }
+    public PeriodoDTO findById(Long id){
+        isNull(id);
+        final Periodo periodo = this.periodoRepository.findById(id).orElse(null);
+        return toPeriodoDTO(periodo);
+    }
+    @Transactional(rollbackFor = Exception.class)
+    public PeriodoDTO create (PeriodoDTO periodo){
+        validationPeriodoDTO(periodo);
+        return toPeriodoDTO(periodoRepository.save(toPeriodo(periodo)));
+    }
+    @Transactional(rollbackFor = Exception.class)
+    public PeriodoDTO update (PeriodoDTO periodo){
+        isNull(periodo.getId());
+        validationPeriodoDTO(periodo);
+        return toPeriodoDTO(periodoRepository.save(toPeriodo(periodo)));
+    }
+    @Transactional(rollbackFor = Exception.class)
+    public void delete(Long id){
+        isNull(id);
+        periodoRepository.deleteById(id);
+    }
+
+}
